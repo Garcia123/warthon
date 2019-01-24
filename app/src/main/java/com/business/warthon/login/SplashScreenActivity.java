9437c7a06edcf8470251e3ffbcdad443ab953b16
jose garcia <newmoon798@gmail.com>
2019-01-23 23:37:18 -0500

    se esta realizando la vista de login

--- app/src/main/java/com/business/warthon/login/SplashScreenActivity.java
+++ app/src/main/java/com/business/warthon/login/SplashScreenActivity.java
@@ -1,5 +1,6 @@
 package com.business.warthon.login;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -8,5 +9,8 @@ public class SplashScreenActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Intent intent = new Intent(SplashScreenActivity.this, LoginActivity.class);
+        startActivity(intent);
+        finish();
     }
 }
