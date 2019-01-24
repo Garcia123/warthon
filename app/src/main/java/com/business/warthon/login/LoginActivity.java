0d7bb5c6e2f35f6c4d11b95653f367c2505aabae
jose garcia <newmoon798@gmail.com>
2019-01-24 17:59:06 -0500

    se realizo las pantallas de bienvenida, login y registros

--- app/src/main/java/com/business/warthon/login/LoginActivity.java
+++ app/src/main/java/com/business/warthon/login/LoginActivity.java
@@ -1,15 +1,28 @@
 package com.business.warthon.login;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 import com.business.warthon.R;
 
 public class LoginActivity extends AppCompatActivity {
 
+    TextView txtLinkRegistrar;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        this.txtLinkRegistrar = findViewById(R.id.txtLinkRegistrar);
+        this.txtLinkRegistrar.setOnClickListener(this::registrarUsurio);
+    }
+
+    private void registrarUsurio(View view) {
+        Intent intent = new Intent(this, RegistrarActivity.class);
+        startActivity(intent);
     }
 }
---
+++ app/src/main/java/com/business/warthon/login/RegistrarActivity.java
@@ -0,0 +1,23 @@
+package com.business.warthon.login;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+
+import com.business.warthon.R;
+
+public class RegistrarActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_registrar);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+    }
+
+}
