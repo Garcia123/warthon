1dd78afd08516d70bb99ebc29f88e5c25044ddb0
jose garcia <newmoon798@gmail.com>
2019-01-25 18:05:11 -0500

    se esta avanzando la pantalla main

--- app/src/main/java/com/business/warthon/login/LoginActivity.java
+++ app/src/main/java/com/business/warthon/login/LoginActivity.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 import com.business.warthon.R;
@@ -11,6 +12,7 @@ import com.business.warthon.R;
 public class LoginActivity extends AppCompatActivity {
 
     TextView txtLinkRegistrar;
+    Button btnIngesar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,6 +21,14 @@ public class LoginActivity extends AppCompatActivity {
 
         this.txtLinkRegistrar = findViewById(R.id.txtLinkRegistrar);
         this.txtLinkRegistrar.setOnClickListener(this::registrarUsurio);
+
+        this.btnIngesar = findViewById(R.id.btnIngesar);
+        this.btnIngesar.setOnClickListener(this::ingresarSistema);
+    }
+
+    private void ingresarSistema(View view) {
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
     }
 
     private void registrarUsurio(View view) {
